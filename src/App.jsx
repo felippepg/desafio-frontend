e6aa58bdd66d3ef9be0983705e014f44bdf1f7f8
Felippe <felippepg.goncalves@outlook.com.br>
2023-07-16 12:50:28 -0300

    Configurando axios para requisições HTTP

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
+import { useEffect } from 'react';
+import { api } from './services/api';
+
 function App() {
+  useEffect(() => {
+    api
+      .get('transacao')
+      .then((response) => console.log(response.data))
+      .catch((error) => console.log(error));
+  });
   return (
     <>
       <p>Click on the Vite and React logos to learn more</p>
