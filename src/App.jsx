f4083dd96454c4b2ef32386ac86288897a5a3bdc
Felippe <felippepg.goncalves@outlook.com.br>
2023-07-16 13:32:51 -0300

    Ajustando layout para os campos digitaveis

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,15 @@ function App() {
   return (
     <div className="bg-red-500 flex flex-col items-center justify-center h-screen w-screen">
       <div className="bg-white flex flex-col w-3/4 h-3/4">
-        <div className="bg-blue-500 flex justify-between p-4">
-          <input className="bg-orange-500 w-1/5" type="text" />
-          <input className="bg-orange-500 w-1/5" type="text" />
-          <input className="bg-orange-500 w-2/5" type="text" />
+        <div className="bg-blue-500 flex flex-col justify-between p-4 md:flex-row">
+          <input className="bg-orange-500 mb-2 md:w-1/5 md:mb-0" type="text" />
+          <input className="bg-orange-500 mb-2 md:w-1/5 md:mb-0" type="text" />
+          <input className="bg-orange-500 mb-2 md:w-2/5 md:mb-0" type="text" />
         </div>
 
-        <div className="flex justify-end pr-4">
+        <div className="flex justify-center md:justify-end pr-4 pl-4">
           <input
-            className="bg-orange-500 w-1/5 mt-2 mb-2 p-1"
+            className="bg-orange-500 w-3/5 md:w-1/5 mt-2 mb-2 p-1"
             type="button"
             value="pesquisar"
           />
